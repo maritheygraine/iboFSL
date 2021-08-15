6680c80b1a67f3d5756dc0e009e50c7bcd308dca
strongforce1 <fporquillo18@gmail.com>
2021-08-16 02:08:45 +0800

    Add recognition fragment

---
+++ app/src/main/java/com/techknow/bigkas/Recognition.java
@@ -0,0 +1,23 @@
+package com.techknow.bigkas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.techknow.bigkas.base.BaseFragment;
+
+public class Recognition extends BaseFragment {
+
+   public Recognition() {
+       super(R.layout.fragment_recognition);
+   }
+
+    @Override
+    public void initView(View view) {
+
+    }
+}
