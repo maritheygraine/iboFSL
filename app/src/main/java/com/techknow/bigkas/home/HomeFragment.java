ea0fdd28fb2ad3250924fb6eb668fb8f94d80835
strongforce1 <fporquillo18@gmail.com>
2021-08-16 02:39:59 +0800

    Fix all with popup menu

--- app/src/main/java/com/techknow/bigkas/home/HomeFragment.java
+++ app/src/main/java/com/techknow/bigkas/home/HomeFragment.java
@@ -1,12 +1,15 @@
 package com.techknow.bigkas.home;
 
-import android.os.Bundle;
+import static androidx.navigation.fragment.NavHostFragment.*;
+
+import android.annotation.SuppressLint;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.PopupMenu;
+import android.widget.Toast;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.annotation.IdRes;
+import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.techknow.bigkas.R;
@@ -31,13 +34,33 @@ public class HomeFragment extends BaseFragment implements MenuItem.OnMenuItemCli
     private void setPopupMenu(final View hamburger) {
         final PopupMenu menu = new PopupMenu(requireContext(), hamburger);
         menu.inflate(R.menu.item_memu);
-
+        menu.setOnMenuItemClickListener(this::onMenuItemClick);
         hamburger.setOnClickListener(e -> menu.show());
     }
 
+    @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onMenuItemClick(MenuItem menuItem) {
         // dito mo lagay yung mga ipapalabas mo na items
+        switch (menuItem.getItemId()) {
+            case R.id.nav_home:
+                changeFragment(R.id.home_to_about);
+                break;
+            case R.id.nav_recognition:
+                showToast("Check mo resource -> navigation folder -> nav_graph.xml");
+                break;
+            case R.id.nav_fingerspelling:
+                showToast("Then hover mo yung phone na may color green na + sign then add ka ng destination.");
+                break;
+        }
         return false;
+    }
+
+    private void changeFragment(@IdRes int navId) {
+        findNavController(this).navigate(navId);
+    }
+
+    private void showToast(final String message) {
+        Toast.makeText(requireContext().getApplicationContext(), message, Toast.LENGTH_LONG).show();
     }
 }
