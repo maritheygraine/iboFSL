b834e19773a40b5f2b01750051c6d2ebf5edd832
strongforce1 <fporquillo18@gmail.com>
2021-08-16 02:00:34 +0800

    Remove unused classes

--- app/src/main/java/com/techknow/bigkas/About.java
+++ app/src/main/java/com/techknow/bigkas/About.java
@@ -1,4 +1,4 @@
-package com.example.bigkas;
+package com.techknow.bigkas;
 
 import android.os.Bundle;
 
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link Recognition#newInstance} factory method to
+ * Use the {@link About#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class Recognition extends Fragment {
+public class About extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class Recognition extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public Recognition() {
+    public About() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class Recognition extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment Recognition.
+     * @return A new instance of fragment About.
      */
     // TODO: Rename and change types and number of parameters
-    public static Recognition newInstance(String param1, String param2) {
-        Recognition fragment = new Recognition();
+    public static About newInstance(String param1, String param2) {
+        About fragment = new About();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class Recognition extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_recognition, container, false);
+        return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
---
+++ app/src/main/java/com/techknow/bigkas/base/BaseFragment.java
@@ -0,0 +1,38 @@
+package com.techknow.bigkas.base;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public abstract class BaseFragment extends Fragment {
+
+    private final @LayoutRes int layout;
+
+    public BaseFragment(final @LayoutRes int layout) {
+        this.layout = layout;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater,
+            @Nullable ViewGroup container,
+            @Nullable Bundle savedInstanceState
+    ) {
+        return inflater.inflate(layout, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initView(view);
+    }
+
+    public abstract void initView(final View view);
+}
