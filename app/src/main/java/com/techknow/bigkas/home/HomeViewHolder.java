c1f4c065776f1740d07855e18e285bc9e60a4fdd
strongforce1 <fporquillo18@gmail.com>
2021-09-07 02:56:42 +0800

    Remove inside nested static adapter class

---
+++ app/src/main/java/com/techknow/bigkas/home/HomeViewHolder.java
@@ -0,0 +1,28 @@
+package com.techknow.bigkas.home;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.techknow.bigkas.R;
+import com.techknow.bigkas.base.Binder;
+
+public class HomeViewHolder extends RecyclerView.ViewHolder implements Binder<String> {
+
+    private final TextView description;
+    private final TextView number;
+
+    public HomeViewHolder(@NonNull View itemView) {
+        super(itemView);
+        description = itemView.findViewById(R.id.description);
+        number = itemView.findViewById(R.id.number);
+    }
+
+    @Override
+    public void bind(String data) {
+        this.description.setText(data);
+        this.number.setText(String.valueOf(getBindingAdapterPosition() + 1));
+    }
+}
