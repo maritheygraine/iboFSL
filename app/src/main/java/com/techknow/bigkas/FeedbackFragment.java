1d94e146e6c71fc16d8343e546200defe31e0029
iorikaze <iorikaze@gmail.com>
2021-09-28 22:44:09 +0800

    Update FeedbackFragment.java

--- app/src/main/java/com/techknow/bigkas/FeedbackFragment.java
+++ app/src/main/java/com/techknow/bigkas/FeedbackFragment.java
@@ -31,14 +31,17 @@ public class FeedbackFragment extends BaseFragment {
                 assert body.getText() != null;
 
                 String s = String.valueOf(ratingBar.getRating());
+                /**
+                 * Making an email in-app then redirect it to an email client
+                 */
                 Intent sendMail = new Intent();
                 sendMail.setData(Uri.parse("mailto:"));
                 final Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"techknow.feutech@gmail.com"});
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Customer Feedback");
+                intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"techknow.feutech@gmail.com"});  // receiver of the email
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Customer Feedback");    // subject of the email
                 intent.putExtra(Intent.EXTRA_TEXT, "My rating of Bigkas App is "+s+"\n\n"+body.getText().toString());
                 intent.setSelector( sendMail );
-                startActivity(Intent.createChooser(intent, "Choose an Email Client"));
+                startActivity(Intent.createChooser(intent, "Choose an Email Client"));  // make user choose an email client like gmail/ymail
             }
         });
     }
